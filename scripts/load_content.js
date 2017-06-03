22bcdebbbe916de8d32a968d31a8d260b007f731
kitti <kitti@DESKTOP-OEORV2A>
2017-06-03 01:39:53 -0400

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

---
+++ scripts/load_content.js
@@ -0,0 +1,32 @@
+
+function loadContent(url, destino, qs) { 
+    var spinner = "<span class='spinner'>&nbsp;<img src='img/loading_facebook.gif' width='160' height='24' alt='Cargando' /> </span>";
+    var destino = jQuery('#'+destino);	
+    destino.html(spinner);
+	jQuery.ajax({
+		type:"get",
+		url:url,
+		dataType:'html',
+		data: qs,
+		cache:false,
+		timeout:30000,
+		error:function(xhr, ts, et){
+			alert("Error al cargar la pagina");
+			destino.html('');
+		},
+		success:function(data, ts){
+			window.setTimeout(function(){
+					destino.html('');
+					destino.html(data);
+				}, 500);
+		}
+	});
+}
+   function agregarPublicacion()
+{
+    var contenido = document.getElementById('txt_buscar').value;
+    loadContent('relleno.php','izq_contenedor','publicacion='+contenido);  
+    
+    document.getElementById('txt_buscar').value="";
+    
+}
